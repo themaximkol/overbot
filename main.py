097acd7f571896c2e1b46c1f0367f12726e20372
themaximkol <42844850+themaximkol@users.noreply.github.com>
2023-10-28 18:36:20 +0200

    v. 2.1.5

--- main.py
+++ main.py
@@ -23,74 +23,61 @@ class User:
     @staticmethod
     @with_cursor
     def get_my_roles(cursor, user_id):
-        cursor.execute("SELECT roles.name FROM roles JOIN user_roles ON roles.id = user_roles.role_id "
-                       "WHERE user_roles.user_id=?", (user_id,))
-        rows = cursor.fetchall()
-        return [row[0] for row in rows]
+        cursor.execute("""
+            SELECT roles.name 
+            FROM roles 
+            JOIN user_roles ON roles.id = user_roles.role_id 
+            WHERE user_roles.user_id=?
+        """, (user_id,))
+        return [row[0] for row in cursor.fetchall()]
 
     @staticmethod
     @with_cursor
     def remove_user_role(cursor, user_id, command):
-        # Find the alias_id
-        cursor.execute("SELECT id FROM aliases WHERE alias=?", (command,))
-        alias_id = cursor.fetchone()
+        alias_id = cursor.execute("SELECT id FROM aliases WHERE alias=?", (command,)).fetchone()
         if not alias_id:
             return False
 
-        # Find the role_id
-        cursor.execute("SELECT role_id FROM role_alias WHERE alias_id=?", (alias_id[0],))
-        role_id = cursor.fetchone()
+        role_id = cursor.execute("SELECT role_id FROM role_alias WHERE alias_id=?", (alias_id[0],)).fetchone()
         if not role_id:
             return False
 
-        # Remove the role
         cursor.execute("DELETE FROM user_roles WHERE user_id=? AND role_id=?", (user_id, role_id[0]))
         return True
 
     @staticmethod
     @with_cursor
     def add_user_role(cursor, user_id, command):
-        # Find the alias_id
-        cursor.execute("SELECT id FROM aliases WHERE alias=?", (command,))
-        alias_id = cursor.fetchone()
+        alias_id = cursor.execute("SELECT id FROM aliases WHERE alias=?", (command,)).fetchone()
         if not alias_id:
             return False
 
-        # Find the role_id
-        cursor.execute("SELECT role_id FROM role_alias WHERE alias_id=?", (alias_id[0],))
-        role_id = cursor.fetchone()
+        role_id = cursor.execute("SELECT role_id FROM role_alias WHERE alias_id=?", (alias_id[0],)).fetchone()
         if not role_id:
             return False
 
-        # Check if the user already has this role
-        cursor.execute("SELECT * FROM user_roles WHERE user_id=? AND role_id=?", (user_id, role_id[0]))
-        existing_entry = cursor.fetchone()
-        if existing_entry:
-            return False
+        if not cursor.execute("SELECT * FROM user_roles WHERE user_id=? AND role_id=?",
+                              (user_id, role_id[0])).fetchone():
+            cursor.execute("INSERT INTO user_roles (user_id, role_id) VALUES (?, ?)", (user_id, role_id[0]))
+            return True
 
-        # Add the role to the user
-        cursor.execute("INSERT INTO user_roles (user_id, role_id) VALUES (?, ?)", (user_id, role_id[0]))
-        return True
+        return False
 
     @staticmethod
     @with_cursor
     def get_user_roles_by_username(cursor, username):
-        # Step 1: Check if the user with the given username exists
-        cursor.execute("SELECT id FROM user WHERE username=?", (username,))
-        user_id = cursor.fetchone()
-
+        user_id = cursor.execute("SELECT id FROM user WHERE username=?", (username,)).fetchone()
         if not user_id:
-            return None  # User with the provided username doesn't exist
+            return None
 
-        user_id = user_id[0]  # Extracting the user ID from the result
-
-        # Step 2: Retrieve roles associated with the user
-        cursor.execute("SELECT roles.name FROM roles "
-                       "JOIN user_roles ON roles.id = user_roles.role_id "
-                       "WHERE user_roles.user_id=?", (user_id,))
-
-        rows = cursor.fetchall()
-        return [row[0] for row in rows]  # Return a list of roles
+        user_id = user_id[0]
+        cursor.execute("""
+            SELECT roles.name 
+            FROM roles 
+            JOIN user_roles ON roles.id = user_roles.role_id 
+            WHERE user_roles.user_id=?
+        """, (user_id,))
+        return [row[0] for row in cursor.fetchall()]
 
     # ---------------------------------------------
     def print_emoji(self):
@@ -99,48 +86,33 @@ class User:
     @staticmethod
     @with_cursor
     def get_usr_emoji(cursor, user_id):
-
         cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,))
-        row = cursor.fetchone()[0].split(",")
-
-        return row
+        return cursor.fetchone()[0].split(",")
 
     @staticmethod
     @with_cursor
     def add_usr_emoji(cursor, user_id, new_emoji):
-
-        cursor.execute("SELECT id FROM user WHERE id=?", (user_id,))
-        user_exists = cursor.fetchone()
-
+        user_exists = cursor.execute("SELECT id FROM user WHERE id=?", (user_id,)).fetchone()
         if user_exists:
-            cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,))
-            current_emojis = cursor.fetchone()[0]
+            current_emojis = cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,)).fetchone()[0]
 
             emojis_list = current_emojis.split(',')
-            if len(emojis_list) < 10:
-                if new_emoji not in emojis_list:
-                    emojis_list.append(new_emoji)
-                    updated_emojis = ','.join(emojis_list)
+            if len(emojis_list) < 10 and new_emoji not in emojis_list:
+                emojis_list.append(new_emoji)
+                updated_emojis = ','.join(emojis_list)
 
-                    cursor.execute("UPDATE user SET emoji=? WHERE id=?", (updated_emojis, user_id))
-                    return True
-            return False
+                cursor.execute("UPDATE user SET emoji=? WHERE id=?", (updated_emojis, user_id))
+                return True
         return False
 
     @staticmethod
     @with_cursor
     def remove_usr_emoji(cursor, user_id, emoji_to_remove):
-
-        cursor.execute("SELECT id FROM user WHERE id=?", (user_id,))
-        user_exists = cursor.fetchone()
-
+        user_exists = cursor.execute("SELECT id FROM user WHERE id=?", (user_id,)).fetchone()
         if user_exists:
-            cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,))
-            current_emojis = cursor.fetchone()[0]
+            current_emojis = cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,)).fetchone()[0]
 
             emojis_list = current_emojis.split(',')
-            print(emojis_list)
-            print(len(emojis_list))
             if emoji_to_remove in emojis_list:
                 emojis_list.remove(emoji_to_remove)
                 updated_emojis = ','.join(emojis_list)
@@ -152,9 +124,8 @@ class User:
     @staticmethod
     @with_cursor
     def get_random_users(cursor):
-        cursor.execute("SELECT id FROM user ORDER BY RANDOM() LIMIT 4")
-        users_data = cursor.fetchall()
-        return users_data
+        cursor.execute("SELECT id, username FROM user ORDER BY RANDOM() LIMIT 4")
+        return cursor.fetchall()
 
 
 def get_role_id(role_name, cursor):
@@ -298,7 +269,7 @@ def handle_view_roles(message):
 @bot.message_handler(commands=['baka'])
 def handle_baka(message):
     random_users = [user_id for user_id, in User.get_random_users()]
-    
+
     response = "Ви наче шарите\n\n" + " ".join(
         [f'<a href="tg://user?id={user}">{random.choice(User.get_usr_emoji(user))}</a>' for user in random_users[:5]]
     )

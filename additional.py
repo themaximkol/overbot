15752a9e598da97539f9617939b60cc7ffab6c10
themaximkol <42844850+themaximkol@users.noreply.github.com>
2023-11-01 00:10:45 +0100

    v. 2.5

--- additional.py
+++ additional.py
@@ -3,35 +3,3 @@ import sqlite3 as sql
 
 with open("token.txt", 'r') as file:
     token = file.read()
-
-
-def with_cursor(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        conn = sql.connect('bot.db')
-        cursor = conn.cursor()
-
-        result = func(cursor, *args, **kwargs)
-
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return result
-
-    return wrapper
-
-
-@with_cursor
-def load_games_aliases(cursor):
-    cursor.execute("SELECT * FROM roles")
-    rows = cursor.fetchall()
-    games = {row[0]: row[1] for row in rows}
-
-    cursor.execute("SELECT * FROM aliases")
-    rows = cursor.fetchall()
-    aliases = {row[0]: row[1] for row in rows}
-
-    return games, aliases
-
-
-games, aliases = load_games_aliases()
--- main.py
+++ main.py
@@ -1,140 +1,156 @@
 import telebot
 import random
 import emoji as emj
+from additional import token
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 
-from datetime import datetime
-from additional import token, with_cursor, games, aliases
-
+engine = create_engine('sqlite:///bot.db')
+Base = declarative_base()
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 bot = telebot.TeleBot(token)
 print("RUNNING")
 
 
-class User:
-    def __init__(self, usr_id="000000000", username="@user", emoji=None):
-        if emoji is None:
-            self.emoji = ["😄"]
+class RoleAlias(Base):
+    __tablename__ = 'role_alias'
+    id = Column(Integer, primary_key=True)
+    alias_id = Column(Integer, ForeignKey('aliases.id'))
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+
+class Alias(Base):
+    __tablename__ = 'aliases'
+    id = Column(Integer, primary_key=True)
+    alias = Column(String, unique=True)
+
+    @classmethod
+    def get_all_aliases(cls):
+        aliases = session.query(Alias).all()
+        return [alias.alias for alias in aliases]
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    users = relationship('User', secondary='user_roles', back_populates='roles')
+
+    @classmethod
+    def get_users_cmd(cls, alias_name, usr_id):
+        alias = session.query(Alias).filter(Alias.alias == alias_name).first()
+        if alias:
+            role = session.query(Role).join(RoleAlias).filter(RoleAlias.alias_id == alias.id).first()
+            if role:
+                return [user.id for user in role.users]
+        return []
+
+    @classmethod
+    def get_role_name(cls, alias_name):
+        alias = session.query(Alias).filter(Alias.alias == alias_name).first()
+        if alias:
+            role = session.query(Role).join(RoleAlias).filter(RoleAlias.alias_id == alias.id).first()
+            return role.name if role else None
+
+
+class UserRole(Base):
+    __tablename__ = 'user_roles'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+
+class UserEmoji(Base):
+    __tablename__ = 'user_emojis'
+    id = Column(Integer, primary_key=True)
+    emoji = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='emojis')
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    roles = relationship('Role', secondary='user_roles',
+                         primaryjoin='User.id == user_roles.c.user_id',
+                         secondaryjoin='Role.id == user_roles.c.role_id',
+                         back_populates='users')
+
+    emojis = relationship('UserEmoji', back_populates='user')
+
+    def add_role(self, alias_name):
+        if self.id in Role.get_users_cmd(alias_name, self.id): return False
+        alias = session.query(Alias).filter(Alias.alias == alias_name).first()
+        if alias:
+            role = session.query(Role).join(RoleAlias).filter(RoleAlias.alias_id == alias.id).first()
+            if role:
+                self.roles.append(role)
+                session.commit()
+                return True
+        return False
+
+    def remove_role(self, alias_name):
+        if self.id not in Role.get_users_cmd(alias_name, self.id): return False
+        alias = session.query(Alias).filter(Alias.alias == alias_name).first()
+        if alias:
+            role = session.query(Role).join(RoleAlias).filter(RoleAlias.alias_id == alias.id).first()
+            if role:
+                self.roles.remove(role)
+                session.commit()
+                return True
+        return False
+
+    def get_roles(self):
+        return [role.name for role in self.roles]
+
+    @staticmethod
+    def get_roles_usrn(username):
+        user = session.query(User).filter_by(username=username).first()
+        if user:
+            return [role.name for role in user.roles]
         else:
-            self.emoji = emoji.split(",")
-
-        self.id = usr_id
-        self.username = username
+            return []
 
     # ---------------------------------------------
-    @staticmethod
-    @with_cursor
-    def get_my_roles(cursor, user_id):
-        cursor.execute("""
-            SELECT roles.name 
-            FROM roles 
-            JOIN user_roles ON roles.id = user_roles.role_id 
-            WHERE user_roles.user_id=?
-        """, (user_id,))
-        return [row[0] for row in cursor.fetchall()]
+    def get_emojis(self):
+        return [emoji.emoji for emoji in self.emojis]
 
-    @staticmethod
-    @with_cursor
-    def remove_user_role(cursor, user_id, command):
-        alias_id = cursor.execute("SELECT id FROM aliases WHERE alias=?", (command,)).fetchone()
-        if not alias_id:
-            return False
-
-        role_id = cursor.execute("SELECT role_id FROM role_alias WHERE alias_id=?", (alias_id[0],)).fetchone()
-        if not role_id:
-            return False
-
-        cursor.execute("DELETE FROM user_roles WHERE user_id=? AND role_id=?", (user_id, role_id[0]))
-        return True
-
-    @staticmethod
-    @with_cursor
-    def add_user_role(cursor, user_id, command):
-        alias_id = cursor.execute("SELECT id FROM aliases WHERE alias=?", (command,)).fetchone()
-        if not alias_id:
-            return False
-
-        role_id = cursor.execute("SELECT role_id FROM role_alias WHERE alias_id=?", (alias_id[0],)).fetchone()
-        if not role_id:
-            return False
-
-        if not cursor.execute("SELECT * FROM user_roles WHERE user_id=? AND role_id=?",
-                              (user_id, role_id[0])).fetchone():
-            cursor.execute("INSERT INTO user_roles (user_id, role_id) VALUES (?, ?)", (user_id, role_id[0]))
+    def add_emoji(self, emoji):
+        if not emj.is_emoji(emoji): return False
+        if len(self.emojis) < 10 and emoji not in [e.emoji for e in self.emojis]:
+            new_emoji = UserEmoji(emoji=emoji)
+            self.emojis.append(new_emoji)
+            session.commit()
             return True
-
         return False
 
-    @staticmethod
-    @with_cursor
-    def get_user_roles_by_username(cursor, username):
-        user_id = cursor.execute("SELECT id FROM user WHERE username=?", (username,)).fetchone()
-        if not user_id:
-            return None
-
-        user_id = user_id[0]
-        cursor.execute("""
-            SELECT roles.name 
-            FROM roles 
-            JOIN user_roles ON roles.id = user_roles.role_id 
-            WHERE user_roles.user_id=?
-        """, (user_id,))
-        return [row[0] for row in cursor.fetchall()]
-
-    # ---------------------------------------------
-    def print_emoji(self):
-        return random.choice(self.emoji)
-
-    @staticmethod
-    @with_cursor
-    def get_usr_emoji(cursor, user_id):
-        cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,))
-        return cursor.fetchone()[0].split(",")
-
-    @staticmethod
-    @with_cursor
-    def add_usr_emoji(cursor, user_id, new_emoji):
-        user_exists = cursor.execute("SELECT id FROM user WHERE id=?", (user_id,)).fetchone()
-        if user_exists:
-            current_emojis = cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,)).fetchone()[0]
-
-            emojis_list = current_emojis.split(',')
-            if len(emojis_list) < 10 and new_emoji not in emojis_list:
-                emojis_list.append(new_emoji)
-                updated_emojis = ','.join(emojis_list)
-
-                cursor.execute("UPDATE user SET emoji=? WHERE id=?", (updated_emojis, user_id))
+    def remove_emoji(self, emoji):
+        if not emj.is_emoji(emoji): return False
+        for e in self.emojis:
+            if e.emoji == emoji:
+                self.emojis.remove(e)
+                session.commit()
                 return True
         return False
 
     @staticmethod
-    @with_cursor
-    def remove_usr_emoji(cursor, user_id, emoji_to_remove):
-        user_exists = cursor.execute("SELECT id FROM user WHERE id=?", (user_id,)).fetchone()
-        if user_exists:
-            current_emojis = cursor.execute("SELECT emoji FROM user WHERE id=?", (user_id,)).fetchone()[0]
-
-            emojis_list = current_emojis.split(',')
-            if emoji_to_remove in emojis_list:
-                emojis_list.remove(emoji_to_remove)
-                updated_emojis = ','.join(emojis_list)
-
-                cursor.execute("UPDATE user SET emoji=? WHERE id=?", (updated_emojis, user_id))
-                return True
-        return False
+    def get_emojis_usrn(username):
+        user = session.query(User).filter(User.username == username).first()
+        if user:
+            return [emoji.emoji for emoji in user.emojis]
+        else:
+            return []
 
     @staticmethod
-    @with_cursor
-    def get_random_users(cursor):
-        cursor.execute("SELECT id, username FROM user ORDER BY RANDOM() LIMIT 4")
-        return cursor.fetchall()
-
-
-def get_role_id(role_name, cursor):
-    cursor.execute(
-        "SELECT role_alias.role_id FROM aliases JOIN role_alias ON aliases.id = role_alias.alias_id WHERE "
-        "aliases.alias = ?",
-        (role_name,))
-    role_id = cursor.fetchone()
-    return role_id[0] if role_id else None
+    def get_emojis_id(usr_id):
+        user = session.query(User).filter(User.id == usr_id).first()
+        if user:
+            return [emoji.emoji for emoji in user.emojis]
+        else:
+            return []
 
 
 def get_text(message):
@@ -145,7 +161,8 @@ def text(game, message):
     command_parts = get_text(message)
 
     if not command_parts:
-        response = f"{games[game]}" + "\n\n"
+
+        response = game + "\n\n"
     else:
         response = " ".join(command_parts) + "\n\n"
 
@@ -154,100 +171,63 @@ def text(game, message):
     return response
 
 
-@bot.message_handler(commands=list(aliases.values()))
-@with_cursor
-def handle_game_command(cursor, message):
+@bot.message_handler(commands=Alias.get_all_aliases())
+def handle_game_command(message):
     command_parts = message.text.split()
     command = command_parts[0][1:]
-    real_command = aliases.get(command, command)
 
-    role_id = get_role_id(real_command, cursor)
+    users = [user for user in Role.get_users_cmd(command, message.from_user.id) if user != str(message.from_user.id)]
+    role = Role.get_role_name(command)
 
-    cursor.execute(
-        "SELECT id, username, emoji FROM user WHERE ? IN (SELECT role_id FROM user_roles WHERE user_id=user.id)",
-        (role_id,))
+    response = text(role, message=message)
 
-    users_data = cursor.fetchall()
-
-    users = [User(user_id, username, emoji) for user_id, username, emoji in users_data if
-             user_id != str(message.from_user.id)]
-
-    response_text = text(role_id, message=message)
-    first_response = response_text + " ".join(
-        [f'<a href="tg://user?id={user.id}">{user.print_emoji()}</a>' for user in users[:5]]
+    response += " ".join(
+        [f'<a href="tg://user?id={user}">{random.choice(User.get_emojis_id(user))}</a>' for user in
+         users[:5]]
     )
 
-    bot.reply_to(message, first_response, parse_mode='HTML')
+    bot.reply_to(message, response, parse_mode='HTML')
 
     if len(users) > 5:
-        second_response = "".join(
-            [f'<a href="tg://user?id={user.id}">{user.print_emoji()}</a>ㅤ' for user in users[5:]]
-        )
+        while len(users) > 5:
+            users = users[5:]
+            second_response = "".join(
+                [f'<a href="tg://user?id={user}">{random.choice(User.get_emojis_id(user))}</a>ㅤ' for user in
+                 users]
+            )
 
-        if len(users[5:]) >= 2:
-            second_response = second_response[:-1]
+            if len(users) >= 2:
+                second_response = second_response[:-1]
 
-        bot.send_message(message.chat.id, second_response, parse_mode='HTML')
-
-
-# ---------------------------------------------
-@bot.message_handler(commands=['emoji', 'my_emoji'])
-def handle_my_emoji(message):
-    emojis = User.get_usr_emoji(message.from_user.id)
-    response = "Emoji: \n\n" + ' '.join(emojis)
-
-    bot.reply_to(message, response)
+            bot.send_message(message.chat.id, second_response, parse_mode='HTML')
 
 
-@bot.message_handler(commands=['addemoji'])
-def handle_add_emoji(message):
-    new_emj = str(message.text.split()[1])
-
-    if len(new_emj) == 1 and emj.is_emoji(new_emj) and User.add_usr_emoji(message.from_user.id, new_emj):
-        reply = f"Emoji {new_emj} added"
-    else:
-        reply = "Нормально пиши"
-
-    bot.reply_to(message, reply)
-
-
-@bot.message_handler(commands=['rmvemoji'])
-def handle_rmv_emoji(message):
-    old_emj = str(message.text.split()[1])
-
-    if len(old_emj) == 1 and emj.is_emoji(old_emj) and User.remove_usr_emoji(message.from_user.id, old_emj):
-        reply = f"Emoji {old_emj} removed"
-    else:
-        reply = "Нормально пиши"
-
-    bot.reply_to(message, reply)
-
-
-# ---------------------------------------------
 @bot.message_handler(commands=['role', 'roles', 'my_roles', 'my_role'])
 def handle_usr_roles(message):
-    reply = " - ".join(User.get_my_roles(message.from_user.id))
+    user = session.query(User).filter(User.id == message.from_user.id).first()
+    reply = " - ".join(user.get_roles())
     bot.reply_to(message, reply)
 
 
 @bot.message_handler(commands=['rmvrole'])
 def handle_rmv_roles(message):
-    success = User.remove_user_role(message.from_user.id, get_text(message)[0])
-    if success:
-        reply = "Role removed"
+    user = session.query(User).filter(User.id == message.from_user.id).first()
+    role = get_text(message)[0]
+    print(role)
+    if user.remove_role(role):
+        bot.reply_to(message, "REMOVED")
     else:
-        reply = "Role removal failed"
-    bot.reply_to(message, reply)
+        bot.reply_to(message, "Пиши нормально")
 
 
 @bot.message_handler(commands=['addrole'])
 def handle_add_roles(message):
-    success = User.add_user_role(message.from_user.id, get_text(message)[0])
-    if success:
-        reply = "Role added"
+    user = session.query(User).filter(User.id == message.from_user.id).first()
+    role = get_text(message)[0]
+    if user.add_role(role):
+        bot.reply_to(message, "ADDED")
     else:
-        reply = "Role addition failed"
-    bot.reply_to(message, reply)
+        bot.reply_to(message, "Пиши нормально")
 
 
 @bot.message_handler(commands=['viewrole'])
@@ -258,12 +238,56 @@ def handle_view_roles(message):
         bot.reply_to(message, "Пиши нормально")
         return
 
-    roles = User.get_user_roles_by_username(username[0])
+    roles = User.get_roles_usrn(username[0])
 
     if roles is None:
         bot.reply_to(message, "Пиши нормально")
     else:
         bot.reply_to(message, str(username[0]) + ": " + " - ".join(roles))
+
+
+@bot.message_handler(commands=['emoji', 'my_emoji', 'emojis', 'my_emojis'])
+def handle_my_emoji(message):
+    user = session.query(User).filter(User.id == message.from_user.id).first()
+    response = "Emoji: \n\n" + " ".join(user.get_emojis())
+
+    bot.reply_to(message, response)
+
+
+@bot.message_handler(commands=['addemoji'])
+def handle_add_emoji(message):
+    user = session.query(User).filter(User.id == message.from_user.id).first()
+    new_emj = str(message.text.split()[1])
+    if user.add_emoji(new_emj):
+        bot.reply_to(message, "ADDED")
+    else:
+        bot.reply_to(message, "Пиши нормально")
+
+
+@bot.message_handler(commands=['rmvemoji'])
+def handle_rmv_emoji(message):
+    user = session.query(User).filter(User.id == message.from_user.id).first()
+    old_emj = str(message.text.split()[1])
+    if user.remove_emoji(old_emj):
+        bot.reply_to(message, "REMOVED")
+    else:
+        bot.reply_to(message, "Пиши нормально")
+
+
+@bot.message_handler(commands=['viewemoji', 'viewemojis'])
+def handle_view_emojis(message):
+    username = get_text(message)
+
+    if username is None:
+        bot.reply_to(message, "Пиши нормально")
+        return
+
+    roles = User.get_emojis_usrn(username[0])
+
+    if roles is None:
+        bot.reply_to(message, "Пиши нормально")
+    else:
+        bot.reply_to(message, str(username[0]) + ": " + " ".join(roles))
 
 
 @bot.message_handler(commands=['baka'])
@@ -275,50 +299,6 @@ def handle_baka(message):
     )
 
     bot.reply_to(message, response, parse_mode='HTML')
-
-
-# ---------------------------------------------
-
-# @bot.message_handler(commands=['all_birthdays'])
-# def handle_all_birthdays(message):
-#     conn = sql.connect('bot.db')
-#     cursor = conn.cursor()
-#     cursor.execute(
-#         "SELECT id, username, emoji FROM user")
-#
-#     users_data = cursor.fetchall()
-#
-#     users = [User(user_id, username, emoji) for user_id, username, emoji in users_data if
-#              user_id]
-#
-#     sorted_users = sorted(users, key=lambda x: datetime.strptime(x.birthday, '%d-%m'))
-#     birthday_list = [f"{user.username}: {user.birthday}" for user in sorted_users if user.birthday != "01-01"]
-#     response = "\n".join(birthday_list)
-#     bot.reply_to(message, response)
-#
-#     cursor.close()
-#     conn.close()
-#
-# @bot.message_handler(commands=['next_birthdays'])
-# def handle_next_birthdays(message):
-#     today = datetime.today()
-#     sorted_users = sorted(users, key=lambda x: datetime.strptime(x.birthday, '%d-%m'))
-#     next_birthdays = []
-#
-#     for user in sorted_users:
-#         if user.birthday != "01-01":
-#             bday = datetime.strptime(user.birthday, '%d-%m').replace(year=today.year)
-#             if bday < today:
-#                 bday = bday.replace(year=today.year + 1)
-#             next_birthdays.append((user.username, bday))
-#
-#     next_birthdays = sorted(next_birthdays, key=lambda x: x[1])
-#     next_birthdays = next_birthdays[:4]
-#
-#     response = "\n".join([f"{name}: {date.strftime('%d-%m')}" for name, date in next_birthdays])
-#     bot.reply_to(message, response)
-#
-#
 
 
 @bot.message_handler(commands=['pack'])
@@ -333,5 +313,4 @@ def handle_max_command(message):
     bot.reply_to(message, link)
 
 
-# bot.send_message("-1001973817859", "🍺")  # test
 bot.polling(non_stop=True, interval=0)
